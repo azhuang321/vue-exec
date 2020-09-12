eb6766149aed7d555271453abc4f7820f96c4948
azhuang <3499296286@qq.com>
2020-09-12 13:37:57 +0800

    修改bug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -56,6 +56,46 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
+      <div class="ads-box">
+        <a :href="'/#/product/' + item.id" v-for=" (item, index) in adsList " :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
+    </div>
+    <div class="product-box">
+      <div class="container">
+        <h2>手机</h2>
+        <div class="wrapper">
+          <div class="banner-left">
+            <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+          </div>
+          <div class="list-box">
+            <div class="list" v-for="(arr,i) in phoneList" :key="i">
+              <div class="item" v-for="(item,j) in arr" :key="j">
+                <span v-if="j%2==0" class="new-pro">新品</span>
+                <span v-if="j%2!=0" class="kill-pro">秒杀</span>
+                <div class="item-img">
+                  <img :src="item.mainImage" alt="">
+                </div>
+                <div class="item-info">
+                  <h3>{{ item.name }}</h3>
+                  <p>{{ item.subtitle }}</p>
+                  <p class="price" @click="addCart(item.id)">{{ item.price }} 元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
+      </div>
     </div>
     <service-bar></service-bar>
   </div>
@@ -138,7 +178,58 @@ export default {
         }
         ], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
       ]
+      , adsList: [
+        {
+          id: 33,
+          img: '/imgs/ads/ads-1.png'
+        },
+        {
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        },
+        {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        },
+        {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
+        },
+      ]
+      , phoneList: []
     }
+  }
+  , mounted() {
+    this.init();
+  }
+  ,methods:{
+    init(){
+      this.axios.get('/products',{
+        params:{
+          categoryId:100012,
+          pageSize:14
+        }
+      }).then((res = {}) => {
+        this.list = res.list.slice(6,14);
+        this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
+      })
+    },
+    addCart(id){
+      this.showModal = id
+      /*this.axios.post('/carts',{
+        productId:id,
+        selected:true
+      }).then(() => {
+
+      }).catch(()=>{
+        this.showModal = true
+      });*/
+    },
+    goToCart(){
+
+    }
+
+
   }
 }
 </script>
@@ -222,17 +313,120 @@ export default {
     }
     .swiper-container {
       height: 451px;
-
       .swiper-button-prev {
         left: 274px;
       }
-
       img {
         width: 100%;
         height: 100%;
       }
     }
   }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner{
+    margin-bottom: 50px;
+  }
+  .product-box{
+    background: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: $fontF;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper{
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img{
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box{
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child{
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+              display: inline-block;
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+              &.new-pro{
+                background-color: #7ECF68 ;
+              }
+              &.kill-pro{
+                background-color: #E82626 ;
+              }
+            }
+            .item-img{
+              img{
+                width: 100%;
+                height: 195px;
+              }
+            }
+            .item-info{
+              h3{
+                font-size:$fontJ;
+                color:$colorB;
+                line-height:$fontJ;
+                font-weight:bold;
+              }
+              p{
+                color:$colorD;
+                line-height:13px;
+                margin:6px auto 13px;
+              }
+              .price{
+                color:#F20A0A;
+                font-size:$fontJ;
+                font-weight:bold;
+                cursor:pointer;
+                &:after{
+                  @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                  content:' ';
+                  margin-left:5px;
+                  vertical-align: middle;
+                }
+              }
+            }
+          }
+        }
+      }
 
+
+    }
+  }
 }
 </style>
+
+
+
+
+
+
+
+
+
+
